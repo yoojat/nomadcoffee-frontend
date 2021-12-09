aebf7139919029b4aa01b4a92e224aaffde89dea
김태영 <kimtaeyoung@0Taes-Mac.local>
2021-12-10 07:41:30 +0900

    Set Up

---
+++ src/screens/Login.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { darkModeVar } from '../apollo';
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+      <button onClick={() => darkModeVar(true)}>To dark</button>
+      <button onClick={() => darkModeVar(false)}>To light</button>
+    </Container>
+  );
+}
+export default Login;
